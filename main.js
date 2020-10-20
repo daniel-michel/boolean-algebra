1ec91bc76cbb36d5c1d26a0479d913dfe4813c0e
daniel-michel <daniel.michel.bonsweiher@gmail.com>
2020-10-20 16:12:14 +0200

    Removed editor variable.

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 import BooleanAlgebraFormula, { FormulaEditor } from "./boolean-algebra.js";
 
-let editor;
 window.onload = main;
 function main()
 {
-	// editor = new FormulaEditor(document.body, "A => B EQUALS not C & D || E != F and true or false");
-	editor = new FormulaEditor(document.body, "A & B | C");
-	editor = new FormulaEditor(document.body, "A & B | C");
+	// new FormulaEditor(document.body, "A => B EQUALS not C & D || E != F and true or false");
+	new FormulaEditor(document.body, "A & B | C");
+	new FormulaEditor(document.body, "A & B | C");
 }
